de467d12fd11079d431dc25a1e5b7f075f0e188a
Ignacio Bejarano <ignbejarano@gmail.com>
2020-06-11 19:05:10 -0300

    conditional import

--- src/run_case.py
+++ src/run_case.py
@@ -1,9 +1,18 @@
-from cases.taylor_green import TaylorGreen
 import matplotlib.pyplot as plt
-from ast import literal_eval
 import numpy as np
+import sys
+import petsc4py
+petsc4py.init(sys.argv)
+import logging
+import yaml 
 
+OptDB = petsc4py.PETSc.Options()
+case = OptDB.getString('case', False)
 
+if case == 'taylor-green':
+    from cases.taylor_green import TaylorGreen as FemProblem
+    print("imported")
+    
 def generateChart(viscousTime):
     hAx = list()
     vAx = list()
@@ -26,15 +35,45 @@ def generateChart(viscousTime):
     plt.show()
 
 def generateParaviewer():
-    fem = TaylorGreen(ngl=2)
+    fem = FemProblem(ngl=3)
     fem.setUpSolver()
     fem.solveKLETests()
 
 def timeSolving():
-    fem = TaylorGreen(ngl=4)
+    fem = FemProblem(ngl=3)
     fem.setUpSolver()
     fem.setUpTimeSolver()
     fem.startSolver()
     fem.viewer.writeXmf('Taylor-Green')
 
-timeSolving()
+def main():
+    case = OptDB.getString('case', False)
+    log = OptDB.getString('log', 'INFO')
+    runTests = OptDB.getString('test', False)
+
+    logging.basicConfig(level=log.upper() )
+    logger = logging.getLogger("")
+
+    try:
+
+
+        with open(f'src/cases/{case}.yaml') as f:
+            yamlData = yaml.load(f, Loader=yaml.Loader)
+
+    except:
+        logger.info(f"Case '{case}' Not Found")
+        exit()
+
+    if runTests:
+        logger.info(f"Running {runTests} TESTS:  {yamlData['name']} ")
+        generateParaviewer()
+    else:
+        logger.info(f"Running problem:  {yamlData['name']}")
+        timeSolving()
+
+    exit()
+    # self.logger.info(yamlData)
+    # self.caseName = "taylor-green"
+
+if __name__ == "__main__":
+    main()
