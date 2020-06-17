a19b392ce708897646cc3d5a9820e8c6cab1946d
Maite Rodriguez Muñiz <rodriguezmmaite@gmail.com>
2020-06-17 09:31:20 -0300

    fixing

--- src/cases/cavity.py
+++ src/cases/cavity.py
@@ -20,26 +20,26 @@ class Cavity(NoSlip):
 
     def computeInitialCondition(self, startTime):
         allNodes = self.dom.getAllNodes()
-        fvort_coords = lambda coords: self.VortCavity(coords, t=startTime)
+        fvort_coords = lambda coords: self.VortCavity(coords,self.dim_w, t=startTime)
         self.vort = self.dom.applyFunctionVecToVec(allNodes, fvort_coords, self.vort, self.dim_w)
 
 
 
     def applyBoundaryConditions(self, time, bcNodes):
         self.vel.set(0.0)
-        fvel_coords = lambda coords: self.VelCavity(coords, t=time)
+        fvel_coords = lambda coords: self.VelCavity(coords,self.applyBoundaryConditions,self.dim, t=time)
         self.vel = self.dom.applyFunctionVecToVec(bcNodes, fvel_coords, self.vel, self.dim)
 
 
     @staticmethod
-    def VelCavity(coord,t=None):
-        for bc in self.applyBoundaryConditions:
+    def VelCavity(coord,applyBoundaryConditions,dim,t=None):
+        for bc in applyBoundaryConditions:
             if coord[bc[1]] == bc[0][1]:
                 vel= bc[2]
                 return vel
-        vel=[0]*self.dim 
+        vel=[0]*dim 
         return vel
 
     @staticmethod
-    def VortCavity_2D(coord, t=None):
-        return [0]*self.dim_w
+    def VortCavity(coord,dim_w ,t=None):
+        return [0]*dim_w
