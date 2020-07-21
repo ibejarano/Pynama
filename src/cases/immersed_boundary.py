df7666b9911bdc42a12bc964dc6ba0d5d8f5e8c7
Ignacio Bejarano <ignbejarano@gmail.com>
2020-07-21 15:26:18 -0300

    ibm body refactor

--- src/cases/immersed_boundary.py
+++ src/cases/immersed_boundary.py
@@ -8,8 +8,9 @@ from mpi4py import MPI
 from petsc4py import PETSc
 from viewer.paraviewer import Paraviewer
 from solver.ksp_solver import KspSolver
+from domain.immersed_body import Circle, Line
 import matplotlib.pyplot as plt
-
+import yaml
 # ibm imports
 from math import sqrt, sin, pi, ceil
 
@@ -38,61 +39,75 @@ class ImmersedBoundaryStatic(FreeSlip):
         self.h = (self.upper[0] - self.lower[0])/numElements
         if self.ngl == 3:
             self.h /= 2
-        elif self.ngl > 3:
-            raise Exception("High Order nodes not implemented")
+        else:
+            self.h /= 4
         self.body = self.createBody(inputData['body'])
 
     def startSolver(self):
         self.computeInitialCondition(startTime= 0.0)
-        self.vort.set(0.0)
-        self.computeVelocityCorrection(NF=4)
-        self.operator.Curl.mult(self.vel, self.vort)
         self.ts.setSolution(self.vort)
         cds = list()
         clifts = list()
         times = list()
-        for i in range(100):
+        maxSteps = self.ts.getMaxSteps()
+
+        fig, ax1 = plt.subplots()
+        ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+        color = 'tab:red'
+        ax1.set_xlabel('time (s)')
+        ax1.set_ylabel(r'Coef. de arrastre $C_D$', color='red')
+        ax1.tick_params(axis='y', labelcolor=color)
+        color = 'tab:blue'
+        ax2.set_ylabel(r'Coef. de empuje $C_{L}$', color='blue')  # we already handled the x-label with ax1
+        ax2.tick_params(axis='y', labelcolor=color)
+
+        fig.tight_layout()  # otherwise the right y-label is slightly clipped
+        for i in range(maxSteps):
             self.ts.step()
             step = self.ts.getStepNumber()
             time = self.ts.time
             dt = self.ts.getTimeStep()
-            qx , qy = self.computeVelocityCorrection(NF=4)
+            qx , qy, fs = self.computeVelocityCorrection(NF=4)
             cd, cl = self.computeDragForce(qx / dt, qy / dt)
             cds.append(cd)
             clifts.append(cl)
             times.append(time)
             self.operator.Curl.mult(self.vel, self.vort)
+            # self.solveKLE(time, self.vort)
             self.viewer.saveVec(self.vel, timeStep=step)
             self.viewer.saveVec(self.vort, timeStep=step)
             self.viewer.saveStepInXML(step, time, vecs=[self.vel, self.vort])
-            self.logger.info(f"Converged: Step {step:4} | Time {time:.4e} | Cd {cd} | Cl {cl} ")
-            # print(f"printing info {self.ts.getTime()= } {self.ts.getSolveTime()= } {self.ts.getPrevTime() = }")
-            # print(f"printing info {self.ts.getTimeStep()= } {self.ts.getStepNumber()= }")
+            self.logger.info(f"Nodos arafue {fs}  Converged: Step {step:4} | Time {time:.4e} | Cd {cd:.6f} | Cl {cl:.3f}")
             self.ts.setSolution(self.vort)
             self.viewer.writeXmf("ibm-static")
-        # print(times)
-        # print(cds)
-        # print(clifts)
-        plt.figure(figsize=(10,10))
-        plt.plot(times, cds, 'r-', label='c_drag')
-        plt.plot(times, clifts, 'b-', label='c_lift')
-        plt.legend()
-        plt.xlabel("times [s]")
-        plt.ylabel("C_d")
-        plt.grid(True)
+            if i % 50 == 0:
+                ax1.clear()
+                ax2.clear()
+                ax1.set_ylim(0,3.5)
+                ax2.set_ylim(0,3.5)
+                ax1.plot(times, cds, color="red")
+                ax2.plot(times, clifts, color="blue")
+                plt.pause(0.0001)
+            if time > self.ts.getMaxTime():
+                break
         # plt.show()
-        nombre = "pruebitas"
+        data = {"times": times, "cd": cds, "cl": clifts}
+        with open('data.yml', 'w') as outfile:
+            yaml.dump(data, outfile, default_flow_style=False)
+        nombre = "re40"
         plt.savefig(f"cdsclsVtime-{nombre}.png")
 
     def computeDragForce(self, fd, fl):
         U = self.U_ref
-        denom = 0.5 * self.rho * (U**2)
+        denom = 0.5 * (U**2)
         cd = fd/denom
-        return cd, fl/denom
+        return float(cd), float(fl/denom)
 
     def computeInitialCondition(self, startTime):
         self.vort.set(0.0)
         self.solveKLE(startTime, self.vort)
+        self.computeVelocityCorrection(NF=2)
+        self.operator.Curl.mult(self.vel, self.vort)
 
     def setUpSolver(self):
         self.solver = KspSolver()
@@ -128,17 +143,21 @@ class ImmersedBoundaryStatic(FreeSlip):
     def computeVelocityCorrection(self, NF=1):
         fx = 0
         fy = 0
+        aux = self.vel.copy()
         bodyVel = self.body.getVelocity()
         for i in range(NF):
             self.H.mult(self.vel, self.ibm_rhs)
             self.ksp.solve(bodyVel - self.ibm_rhs, self.virtualFlux)
             self.S.mult(self.virtualFlux, self.vel_correction)
-            aux = self.virtualFlux.getArray()
-            fx_part, fy_part = self.body.computeForce(aux)
+            # fx += fx_part
+            # fy += fy_part
+            self.vel += self.vel_correction
+            aux = self.virtualFlux
+            # self.H.multTranspose(self.ibm_rhs, aux)
+            fx_part, fy_part, fs = self.body.computeForce(aux)
             fx += fx_part
             fy += fy_part
-            self.vel += self.vel_correction
-        return abs(fx*self.rho), abs(fy*self.rho)
+        return abs(fx)*self.h**2, abs(fy)*self.h**2, fs
 
     def createBody(self, inp):
         vel = inp['vel']
@@ -169,12 +188,16 @@ class ImmersedBoundaryStatic(FreeSlip):
         eulerCoords = self.dom.getNodesCoordinates(nodes)
         for lagNode in range(lagNodes):
             dirac = self.computeDirac(lagNode, eulerCoords)
+            totNNZ = 0
+            for i in dirac:
+                if i>0:
+                    totNNZ +=1
             for dof in range(self.dim):
                 self.H.setValues(lagNode*self.dim+dof, eulerIndices[dof::self.dim], dirac)
+            self.body.setEulerNodes(lagNode, totNNZ)
 
         self.H.assemble()
         self.S = self.H.copy().transpose()
-
         dl = self.body.getElementLong()
         self.S.scale(dl)
         self.H.scale(self.h**2)
@@ -211,247 +234,8 @@ class ImmersedBoundaryStatic(FreeSlip):
     def computeCentroid(corners):
         return np.mean(corners, axis=0)
 
-
-
 class ImmersedBoundaryDynamic(ImmersedBoundaryStatic):
     def getVorticityCorrection(self, t, finalStep=False):
         """Main function to be called after a Converged Time Step"""
         vort = self.operator.Curl.createVecLeft()
         return vort 
-
-class ImmersedBody:
-    def __init__(self, vel=[0,0]):
-        self.dirac = fourGrid
-        self.__centerDisplacement = [0,0]
-        self.__dl = None
-        self.__vel = vel
-    
-    def setUpDimensions(self):
-        self.firstNode, self.lastNode = self.__dom.getHeightStratum(1)
-        self.coordinates = self.__dom.getCoordinatesLocal()
-        self.coordSection = self.__dom.getCoordinateSection()
-
-    def generateDMPlex(self, coords, cone, dim=1):
-        self.__dom = PETSc.DMPlex().createFromCellList(dim, cone,coords)
-        self.setUpDimensions()
-        points = self.getTotalNodes()
-        ind = [poi*2+dof for poi in range(points) for dof in range(len(self.__vel))]
-        self.__velVec = PETSc.Vec().createMPI(
-            (( points * len(self.__vel), None)))
-        self.__velVec.setValues( ind , np.tile(self.__vel, points) )
-        self.__velVec.assemble()
-
-    def saveVTK(self):
-        viewer = PETSc.Viewer()
-        viewer.createVTK('body.vtk', mode=PETSc.Viewer.Mode.WRITE)
-        viewer.view(self.__dom)
-        viewer.destroy()
-
-    def setCenter(self, val):
-        self.__centerDisplacement = val
-
-    def setElementLong(self, dl, normals):
-        self.__normals = normals
-        self.__dl = dl
-
-    def computeForce(self, q):
-        # fx = q[0]*norm[0] + q[2]*norm[2]
-        # fy = q[1]*norm[1] + q[3]*norm[3]
-        fx = 0
-        fy = 0
-        points = self.getTotalNodes()
-        for poi in range(points):
-            coord = self.getNodeCoordinates(poi)
-            d_x = coord[0] * self.__dl / 0.5
-            d_y = coord[1] * self.__dl / 0.5
-            fx += q[poi*2]*d_x
-            fy += q[poi*2+1]*d_y
-        return fx, fy
-
-    def getVelocity(self):
-        return self.__velVec
-
-    def getElementLong(self):
-        return self.__dl
-
-    def getTotalNodes(self):
-        return self.lastNode - self.firstNode
-
-    def setCaracteristigLong(self, val):
-        self.__L = val
-
-    def getCaracteristicLong(self):
-        return self.__L
-
-    def getNodeCoordinates(self, node):
-        return self.__dom.vecGetClosure(
-            self.coordSection, self.coordinates, node + self.firstNode
-            ) + self.__centerDisplacement
-    
-    def getRegion(self):
-        return None
-
-    def getDiracs(self, dist):
-        dirac = 1
-        for r in dist:   
-            dirac *= self.dirac(abs(r)/self.__dl)
-            dirac /= self.__dl
-        return dirac
-
-class Line(ImmersedBody):
-    def generateBody(self, div, **kwargs):
-        # this can be improved with lower & upper
-        longitud = kwargs['long']
-        coords_x = np.linspace(0, longitud, div)
-        coords_y = np.array([1]*div)
-        coords = np.array([coords_x.T, coords_y.T])
-        dl = longitud / (div-1)
-        cone= list()
-        for i in range(div-1):
-            localCone = [i,i+1]
-            cone.append(localCone)
-
-        self.generateDMPlex(coords.T, cone)
-        self.setCenter(np.array([0,0]))
-        self.setCaracteristigLong(longitud)
-        self.setElementLong(dl, normals=[1])
-
-    def getRegion(self):
-        return 1
-
-class Circle(ImmersedBody):
-    def generateBody(self, dh, **kwargs):
-        r = kwargs['radius']
-        rev = 2*pi
-        n = ceil(rev*r/dh)
-        assert n > 4
-        div = rev/n
-        startAng = 0.1*pi
-        # startAng = 0
-        angles = np.arange(0, rev+div , div)
-        x = r * np.cos(angles + startAng)
-        y = r * np.sin(angles + startAng)
-        coords = list()
-        cone = list()
-        norms = list()
-        for i in range(len(x)-1):
-            localCone = [i,i+1]
-            coords.append([x[i] , y[i]])
-            norms.append([x[i]/r , y[i]/r])
-            cone.append(localCone)
-        cone[-1][-1] = 0
-        dl = sqrt((coords[0][0]-coords[1][0])**2 + (coords[0][1]-coords[1][1])**2)
-
-        self.setCenter(np.array([0,0]))
-        self.setElementLong(dl, normals=norms)
-        self.setCaracteristigLong(r*2)
-        self.radius = r
-        self.generateDMPlex(coords, cone)
-
-    def getRadius(self):
-        return self.radius
-
-    def getRegion(self):
-        dl = self.getElementLong()
-        return self.radius + dl
-
-    def computeVelocity(self, t):
-        velX = 0
-        velY = sin(t/2)
-        # print('Computed Vel Y' , velY)
-        nodes = self.lastNode - self.firstNode
-        for i in range(nodes):
-            velIndex = [i*2 , i*2 +1] 
-            # print('velocity ind', velIndex)
-            vel =  [velX, velY]
-            # print('Vel', vel)
-            self.__velocity.setValues(velIndex, vel, False)
-        # print('Body vel size', self._BodyVelocity.getSize())
-        self.__velocity.assemble()
-
-    def updateCoordinates(self, t):
-        displX = 0
-        displY = t * sin(t/2)
-        self.__centerDisplacement = np.array([displX , displY])
-
-def threeGrid(r):
-    """supports only three cell grids"""
-    if r <=  0.5:
-        return (1 + sqrt(-3*r**2 + 1))/3
-    elif r <= 1.5:
-        return (5 - 3*r - sqrt(-3*(1-r)**2 + 1))/6
-    else:
-        return 0
-
-def linear(r):
-    """Lineal Dirac discretization"""
-    if (r < 1):
-        return (1 - r)
-    else:
-        return 0
-
-def fourGrid(r):
-    if r <=  1:
-        return (3 - 2*r + sqrt(1 + 4*r - 4*r**2))/8
-    elif r <= 2:
-        return (5 - 2*r - sqrt(-7+12*r-4*r**2))/8
-    else:
-        return 0
-
-class EulerNodes:
-    def __init__(self, total, dim):
-        self.__eulerNodes = list()
-        self.__localLagNodes = set()
-        self.__totalNodes = total
-        self.__dim = dim
-
-    def __repr__(self):
-        print(f"Total Nodes in Domain: {self.__totalNodes}")
-        print("Nodes affected by Body")
-        for eul in self.__eulerNodes:
-            print(f"Node Euler: {eul.getNumEuler()} :  {eul.getLagList()}  ")
-
-        print(f"Local Lagrangian num Nodes: {self.__localLagNodes}")
-        return "------"
-
-    def getAffectedNodes(self):
-        return self.__eulerNodes
-
-    def add(self, eul, lag, diracs):
-        euler = EulerNode(eul, lag, diracs)
-        self.__eulerNodes.append(euler)
-        self.__localLagNodes.update(lag)
-
-    def generate_d_nnz(self):
-        d_nnz = [0] * self.__totalNodes * self.__dim
-        for eul in self.__eulerNodes:
-            nodeNum = eul.getNumEuler()
-            for dof in range(self.__dim):
-                d_nnz[nodeNum*2+dof] = eul.getNumLag()
-        return d_nnz
-
-    def getProblemDimension(self):
-        rows = self.__totalNodes * self.__dim
-        cols = len(self.__localLagNodes) * self.__dim
-        return cols, rows
-
-class EulerNode:
-    def __init__(self, num, lag, diracs):
-        self.__num = num
-        self.__lagNodes = lag
-        self.__diracs = diracs
-
-    def __repr__(self):
-        return f"Euler Node: {self.__num}"
-
-    def getLagList(self):
-        return self.__lagNodes
-
-    def getNumLag(self):
-        return len(self.__lagNodes)
-    
-    def getNumEuler(self):
-        return self.__num
-        
-    def getDiracComputed(self):
-        return self.__diracs
---
+++ src/domain/immersed_body.py
@@ -0,0 +1,244 @@
+import numpy as np
+from math import sin , pi , sqrt, ceil
+from petsc4py import PETSc
+
+class ImmersedBody:
+    def __init__(self, vel=[0,0]):
+        self.dirac = fourGrid
+        self.__centerDisplacement = [0,0]
+        self.__dl = None
+        self.__vel = vel
+    
+    def setUpDimensions(self):
+        self.firstNode, self.lastNode = self.__dom.getHeightStratum(1)
+        self.coordinates = self.__dom.getCoordinatesLocal()
+        self.coordSection = self.__dom.getCoordinateSection()
+        self.__lagNodes = [0]*(self.lastNode - self.firstNode)
+
+    def setEulerNodes(self, lagPoi, NNZNodes):
+        self.__lagNodes[lagPoi] = NNZNodes
+
+    def generateDMPlex(self, coords, cone, dim=1):
+        self.__dom = PETSc.DMPlex().createFromCellList(dim, cone,coords)
+        self.setUpDimensions()
+        points = self.getTotalNodes()
+        ind = [poi*2+dof for poi in range(points) for dof in range(len(self.__vel))]
+        self.__velVec = PETSc.Vec().createMPI(
+            (( points * len(self.__vel), None)))
+        self.__velVec.setValues( ind , np.tile(self.__vel, points) )
+        self.__velVec.assemble()
+
+    def saveVTK(self):
+        viewer = PETSc.Viewer()
+        viewer.createVTK('body.vtk', mode=PETSc.Viewer.Mode.WRITE)
+        viewer.view(self.__dom)
+        viewer.destroy()
+
+    def setCenter(self, val):
+        self.__centerDisplacement = val
+
+    def setElementLong(self, dl, normals):
+        self.__normals = normals
+        self.__dl = dl
+
+    def computeForce(self, q):
+        fx = 0
+        fy = 0
+        fouris = 0
+        points = self.getTotalNodes()
+        for poi in range(points):
+            nodes = self.__lagNodes[poi]
+            fx += q[poi*2] * nodes
+            fy += q[poi*2] * 16
+            if 16 - nodes < 0:
+                print("HAY MAS NODOS!")
+            fouris += 16 - nodes
+        return fx, fy, fouris
+
+    def getVelocity(self):
+        return self.__velVec
+
+    def getElementLong(self):
+        return self.__dl
+
+    def getTotalNodes(self):
+        return self.lastNode - self.firstNode
+
+    def setCaracteristigLong(self, val):
+        self.__L = val
+
+    def getCaracteristicLong(self):
+        return self.__L
+
+    def getNodeCoordinates(self, node):
+        return self.__dom.vecGetClosure(
+            self.coordSection, self.coordinates, node + self.firstNode
+            ) + self.__centerDisplacement
+    
+    def getRegion(self):
+        return None
+
+    def getDiracs(self, dist):
+        dirac = 1
+        for r in dist:   
+            dirac *= self.dirac(abs(r)/self.__dl)
+            dirac /= self.__dl
+        return dirac
+
+class Line(ImmersedBody):
+    def generateBody(self, div, **kwargs):
+        # this can be improved with lower & upper
+        longitud = kwargs['long']
+        coords_x = np.linspace(0, longitud, div)
+        coords_y = np.array([1]*div)
+        coords = np.array([coords_x.T, coords_y.T])
+        dl = longitud / (div-1)
+        cone= list()
+        for i in range(div-1):
+            localCone = [i,i+1]
+            cone.append(localCone)
+
+        self.generateDMPlex(coords.T, cone)
+        self.setCenter(np.array([0,0]))
+        self.setCaracteristigLong(longitud)
+        self.setElementLong(dl, normals=[1])
+
+    def getRegion(self):
+        return 1
+
+class Circle(ImmersedBody):
+    def generateBody(self, dh, **kwargs):
+        r = kwargs['radius']
+        rev = 2*pi
+        n = ceil(rev*r/dh)
+        assert n > 4
+        div = rev/n
+        startAng = 0.1*pi
+        # startAng = 0
+        angles = np.arange(0, rev+div , div)
+        x = r * np.cos(angles + startAng)
+        y = r * np.sin(angles + startAng)
+        coords = list()
+        cone = list()
+        norms = list()
+        for i in range(len(x)-1):
+            localCone = [i,i+1]
+            coords.append([x[i] , y[i]])
+            norms.append([x[i]/r , y[i]/r])
+            cone.append(localCone)
+        cone[-1][-1] = 0
+        dl = sqrt((coords[0][0]-coords[1][0])**2 + (coords[0][1]-coords[1][1])**2)
+
+        self.setCenter(np.array([0,0]))
+        self.setElementLong(dl, normals=norms)
+        self.setCaracteristigLong(r*2)
+        self.radius = r
+        self.generateDMPlex(coords, cone)
+
+    def getRadius(self):
+        return self.radius
+
+    def getRegion(self):
+        dl = self.getElementLong()
+        return self.radius + dl
+
+    def computeVelocity(self, t):
+        velX = 0
+        velY = sin(t/2)
+        # print('Computed Vel Y' , velY)
+        nodes = self.lastNode - self.firstNode
+        for i in range(nodes):
+            velIndex = [i*2 , i*2 +1] 
+            # print('velocity ind', velIndex)
+            vel =  [velX, velY]
+            # print('Vel', vel)
+            self.__velocity.setValues(velIndex, vel, False)
+        # print('Body vel size', self._BodyVelocity.getSize())
+        self.__velocity.assemble()
+
+    def updateCoordinates(self, t):
+        displX = 0
+        displY = t * sin(t/2)
+        self.__centerDisplacement = np.array([displX , displY])
+
+def threeGrid(r):
+    """supports only three cell grids"""
+    if r <=  0.5:
+        return (1 + sqrt(-3*r**2 + 1))/3
+    elif r <= 1.5:
+        return (5 - 3*r - sqrt(-3*(1-r)**2 + 1))/6
+    else:
+        return 0
+
+def linear(r):
+    """Lineal Dirac discretization"""
+    if (r < 1):
+        return (1 - r)
+    else:
+        return 0
+
+def fourGrid(r):
+    if r <=  1:
+        return (3 - 2*r + sqrt(1 + 4*r - 4*r**2))/8
+    elif r <= 2:
+        return (5 - 2*r - sqrt(-7+12*r-4*r**2))/8
+    else:
+        return 0
+
+class EulerNodes:
+    def __init__(self, total, dim):
+        self.__eulerNodes = list()
+        self.__localLagNodes = set()
+        self.__totalNodes = total
+        self.__dim = dim
+
+    def __repr__(self):
+        print(f"Total Nodes in Domain: {self.__totalNodes}")
+        print("Nodes affected by Body")
+        for eul in self.__eulerNodes:
+            print(f"Node Euler: {eul.getNumEuler()} :  {eul.getLagList()}  ")
+
+        print(f"Local Lagrangian num Nodes: {self.__localLagNodes}")
+        return "------"
+
+    def getAffectedNodes(self):
+        return self.__eulerNodes
+
+    def add(self, eul, lag, diracs):
+        euler = EulerNode(eul, lag, diracs)
+        self.__eulerNodes.append(euler)
+        self.__localLagNodes.update(lag)
+
+    def generate_d_nnz(self):
+        d_nnz = [0] * self.__totalNodes * self.__dim
+        for eul in self.__eulerNodes:
+            nodeNum = eul.getNumEuler()
+            for dof in range(self.__dim):
+                d_nnz[nodeNum*2+dof] = eul.getNumLag()
+        return d_nnz
+
+    def getProblemDimension(self):
+        rows = self.__totalNodes * self.__dim
+        cols = len(self.__localLagNodes) * self.__dim
+        return cols, rows
+
+class EulerNode:
+    def __init__(self, num, lag, diracs):
+        self.__num = num
+        self.__lagNodes = lag
+        self.__diracs = diracs
+
+    def __repr__(self):
+        return f"Euler Node: {self.__num}"
+
+    def getLagList(self):
+        return self.__lagNodes
+
+    def getNumLag(self):
+        return len(self.__lagNodes)
+    
+    def getNumEuler(self):
+        return self.__num
+        
+    def getDiracComputed(self):
+        return self.__diracs
