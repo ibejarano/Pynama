66b419340decc19a55f98cfbbb9ee8ad5affd09c
Maite Rodriguez Muñiz <rodriguezmmaite@gmail.com>
2020-07-24 14:22:05 -0300

    taylor-green3d

--- src/cases/taylor_green.py
+++ src/cases/taylor_green.py
@@ -90,20 +90,24 @@ class TaylorGreen(FreeSlip):
     def taylorGreenVel_3D(coord, nu ,t=None):
         Lx= 1
         Ly= 1
+        Lz = 1
         Uref = 1
         x_ = 2 * pi * coord[0] / Lx
         y_ = 2 * pi * coord[1] / Ly
-        expon = Uref * exp(-4 * (pi**2) * nu * t * (1.0 / Lx ** 2 + 1.0 / Ly ** 2))
-        vel = [cos(x_) * sin(y_) * expon, -sin(x_) * cos(y_) * expon]
-        return [vel[0], vel[1], 0]
+        z_ = 2 * pi * coord[2] / Lz
+        expon = Uref * exp(-4 * (pi**2) * nu * t * (1.0 / Lx ** 2 + 1.0 / Ly ** 2+ 1.0 / Lz ** 2))
+        vel = [cos(x_) * sin(y_) *sin(z_)*Lx* expon, sin(x_) * cos(y_) *sin(z_)*Ly *expon,-2*sin(x_)* sin(y_) * cos(z_) *Lz* expon]
+        return vel
 
     @staticmethod
     def taylorGreenVort_3D(coord, nu ,t=None):
         Lx= 1
         Ly= 1
+        Lz=1
         Uref = 1
         x_ = 2 * pi * coord[0] / Lx
         y_ = 2 * pi * coord[1] / Ly
-        expon = Uref * exp(-4 * (pi**2) * nu * t * (1.0 / Lx ** 2 + 1.0 / Ly ** 2))
-        vort = -2 * pi * (1.0 / Lx + 1.0 / Ly) * cos(x_) * cos(y_) * expon
-        return [0,0,vort]
+        z_ = 2 * pi * coord[2] / Lz
+        expon = Uref * exp(-4 * (pi**2) * nu * t * (1.0 / Lx ** 2 + 1.0 / Ly ** 2+ 1.0 / Lz ** 2))
+        vort = [-2 * pi * (Ly / Lz + 2* Lz / Ly) * sin(x_) * cos(y_) *cos(z_)* expon,2 * pi * (Lx / Lz + 2* Lz / Lx) * cos(x_) * sin(y_) *cos(z_)* expon,2 * pi * (Ly / Lx - Lx / Ly) * cos(x_) * cos(y_) *sin(z_)* expon]
+        return vort
