d13afca3d89d61a06e68eea6035bc78566e1b2ee
Ignacio Bejarano <ignbejarano@gmail.com>
2020-07-24 17:20:29 -0300

    added Plotter class

--- src/viewer/plotter.py
+++ src/viewer/plotter.py
@@ -1,4 +1,8 @@
 import matplotlib.pyplot as plt
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['ytick.labelsize'] = 12
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['legend.fontsize'] = 14
 
 class DualAxesPlotter:
     availableTypes = ["simple","dual", "semilog", "log"]
@@ -21,12 +25,36 @@ class DualAxesPlotter:
     def updatePlot(self, x ,var1, var2, realTimePlot=False):
         self.ax1.clear()
         self.ax2.clear()
-        self.ax1.set_ylim(0,3.5)
-        self.ax2.set_ylim(0,3.5)
+        self.ax1.set_ylim(0,2)
+        self.ax2.set_ylim(-1,1)
         self.ax1.plot(x, var1, color="red")
         self.ax2.plot(x, var2, color="blue")
         if realTimePlot:
             self.plt.pause(0.0001)
+        self.plt.show()
 
     def savePlot(self, name):
         self.plt.savefig(name+".png")
+
+class Plotter:
+    color = ["red", "blue", "black"]
+    def __init__(self, xlabel: str, ylabel: str):
+        fig, ax1 = plt.subplots()
+        ax1.set_xlabel(xlabel)
+        ax1.set_ylabel(ylabel)
+        fig.tight_layout()
+        self.ax = ax1
+        self.fig = fig
+        self.plt = plt
+
+    def updatePlot(self, x : list , variables: dict, save=False ,**kwargs):
+        self.ax.set(**kwargs)
+        self.ax.grid()
+        for i, var in enumerate(variables):
+            self.ax.plot(x, var['data'], label=var['name'], color=self.color[i])
+        self.plt.legend()
+        
+        if save:
+            self.plt.savefig("static-cd-cl-re80.png")
+        else:
+            self.plt.show()
