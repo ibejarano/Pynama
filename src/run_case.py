7d72b1aee52c241fb27002f325811187a8b9b371
Ignacio Bejarano <ignbejarano@gmail.com>
2020-07-30 21:13:40 -0300

    fix typo

--- src/run_case.py
+++ src/run_case.py
@@ -25,8 +25,8 @@ else:
     print("Case not defined unabled to import")
     exit()
 
-def generateChart(viscousTimes=[0.01,0.05,0.1,0.15,0.2,0.25,0.3,0.4,0.5,0.6,0.7,0.8,0.9]):
-    viscousTime = [0.01 , 0.05 , 0.1, 0.15]
+def generateChart(viscousTime=[0.01,0.05,0.1,0.15,0.2,0.25,0.3,0.4,0.5,0.6,0.7,0.8,0.9]):
+    # viscousTime = [0.01 , 0.05 , 0.1, 0.15]
     hAx = list()
     vAx = list()
     totalNgl = 21
