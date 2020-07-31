767c4e21c66c30e1d438d80a899396d3c03067e8
Ignacio Bejarano <ignbejarano@gmail.com>
2020-07-31 11:48:18 -0300

    fix config generateChart

--- src/run_case.py
+++ src/run_case.py
@@ -25,13 +25,14 @@ else:
     print("Case not defined unabled to import")
     exit()
 
-def generateChart(viscousTime=[0.01,0.05,0.1,0.15,0.2,0.25,0.3,0.4,0.5,0.6,0.7,0.8,0.9]):
+def generateChart(config, viscousTime=[0.01,0.05,0.1,0.15,0.2,0.25,0.3,0.4,0.5,0.6,0.7,0.8,0.9]):
     # viscousTime = [0.01 , 0.05 , 0.1, 0.15]
     hAx = list()
     vAx = list()
     totalNgl = 9
+    plt.figure(figsize=(10,10))
     for i, ngl in enumerate(range(3,totalNgl,1)):
-        fem = FemProblem(ngl=ngl)
+        fem = FemProblem(config, ngl=ngl)
         fem.setUp()
         fem.setUpSolver()
         vAx.append(fem.getKLEError(viscousTimes=viscousTime))
@@ -41,7 +42,6 @@ def generateChart(viscousTime=[0.01,0.05,0.1,0.15,0.2,0.25,0.3,0.4,0.5,0.6,0.7,0
     #hAx = list(range(3,totalNgl,2))
     marker = [',','v','>','<','1','2','3','4','s','p','*','h','+']
     vAxArray = np.array(vAx)
-    plt.figure(figsize=(10,10))
     for i in range(vAxArray.shape[1]):
         plt.loglog(hAx, vAxArray[:,i],'k'+marker[i]+'-', basey=10,label=r'$ \tau = $' + str(viscousTime[i]) ,linewidth =0.5)
         
