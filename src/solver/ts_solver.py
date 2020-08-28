3b8945a57882fc93d3e2cae078daeb38f812d76c
Maite Rodriguez Muñiz <rodriguezmmaite@gmail.com>
2020-08-28 11:00:06 -0300

    test chartkle for few steps

--- src/solver/ts_solver.py
+++ src/solver/ts_solver.py
@@ -6,7 +6,7 @@ class TsSolver(TS):
         self.setProblemType(self.ProblemType.NONLINEAR)  # Should we use LINEAR?
         # self.setEquationType(self.EquationType.ODE_EXPLICIT)
         self.setType(self.Type.RK)
-        # self.setRKType(self.RKType.RK5F)
+        self.setRKType(self.RKType.RK5F)
 
     def setUpTimes(self, sTime, eTime, steps):
         self.setTime(sTime)
