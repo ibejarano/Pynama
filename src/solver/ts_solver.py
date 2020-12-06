7e4da080a1278175f6db322d0131fd994ed1a5d1
Ignacio Bejarano <ignbejarano@gmail.com>
2020-12-06 10:57:05 -0300

    added TSSolver test suite

--- src/solver/ts_solver.py
+++ src/solver/ts_solver.py
@@ -1,18 +1,19 @@
 from petsc4py.PETSc import TS
 
 class TsSolver(TS):
-    def __init__(self, comm):
-        self.create(comm=comm)
-        self.setProblemType(self.ProblemType.NONLINEAR)  # Should we use LINEAR?
+    rk_types = ["3", "5f", "5bs"]
+    def __init__(self):
+        self.create()
+        self.setProblemType(self.ProblemType.NONLINEAR)
         # self.setEquationType(self.EquationType.ODE_EXPLICIT)
-        self.setType(self.Type.RK)
-        self.setRKType(self.RKType.RK5F)
+        self.setRK()
+
+    def setRK(self):
+        self.setType('rk')
+        self.setRKType('5bs')
 
     def setUpTimes(self, sTime, eTime, steps):
         self.setTime(sTime)
-        # ts.setTimeStep(1e-6)
-        # ts.setInitialTimeStep(sTime, (eTime - sTime) / (100 * maxSteps))
-        # ts.setDuration(eTime, max_steps=maxSteps)
         self.setMaxTime(eTime)
         self.setMaxSteps(steps)
         self.setExactFinalTime(self.ExactFinalTime.MATCHSTEP)
@@ -20,13 +21,6 @@ class TsSolver(TS):
         # Sundials doesn't support MATCHSTEP (-ts_exact_final_time INTERPOLATE)
 
     def initSolver(self, rhsFunction, convergedStepFunction):
-        # init variables for solution
-        # self.initKLErhsFunction()
-        # Set rhs function
         self.setRHSFunction(rhsFunction)
-        # ts.setIFunction(self.evalKLEiFunction, self.f)
-        # ts.setIJacobian(self.evalKLEiJacobian, self.Jac)
-
-        # Set fuction to be run after each step
         self.setPostStep(convergedStepFunction)
         self.setFromOptions()
