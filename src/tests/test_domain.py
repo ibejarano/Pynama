b090555595d8dea27aa16af39b5eb64be6c3fcf3
Ignacio Bejarano <ignbejarano@gmail.com>
2020-12-07 10:47:29 -0300

    reimplemented all tests

--- src/tests/test_domain.py
+++ src/tests/test_domain.py
@@ -103,6 +103,14 @@ class TestBoxDMPLEX3D(unittest.TestCase):
         coord= self.dom.getCellCornersCoords(0)
         np_test.assert_array_almost_equal(coords_cell_0, coord, decimal=13)
 
+
+    def test_border_names(self):
+        borderNames = self.dom.getBordersNames()
+
+        assert len(borderNames) == 6
+        for b in borderNames:
+            assert b in ['up', 'down', 'left', 'right', 'front', 'back']
+
     def test_borders_nodes(self):
         totalArc = 28*11
         totalFace = 35*2
@@ -145,84 +153,99 @@ class TestNglIndexing3D(unittest.TestCase):
             total = cornerNodes + edges*(ngl-2) + cells*((ngl-2)**2)
             assert len(dom.getBordersNodes()) == total
 
-    # def test_border_nodes_num(self):
-    #     borderNames = self.doms[0].getBordersNames()
-    #     for dom in self.doms:
-    #         ngl = dom.getNGL()
-    #         for b in borderNames:
-    #             if b in ['up', 'down']:
-    #                 total = 3 + 2*(ngl-2)
-    #                 assert len(dom.getBorderNodes(b)) == total
-    #             else:
-    #                 total = 4 + 3*(ngl-2)
-    #                 assert len(dom.getBorderNodes(b)) == total
+    def test_border_nodes_num(self):
+        borderNames = self.doms[0].getBordersNames()
+        for dom in self.doms:
+            ngl = dom.getNGL()
+            for b in borderNames:
+                if b in ['up', 'down']:
+                    total = 15 + 22*(ngl-2) + 8*((ngl-2)**2)
+                    assert len(dom.getBorderNodes(b)) == total
+                elif b in ['right', 'left']:
+                    total = 20 + 31*(ngl-2) + 12*((ngl-2)**2)
+                    assert len(dom.getBorderNodes(b)) == total
+                elif b in ['front', 'back']:
+                    total = 12 + 17*(ngl-2) + 6*((ngl-2)**2)
+                    assert len(dom.getBorderNodes(b)) == total
+                else:
+                    raise Exception("Not found Border")
 
-# class DomainModTests(unittest.TestCase):
+class DomainModTests2D(unittest.TestCase):
 
-#     def setUp(self):
-#         dim = 2
-#         self.dom2d = DMPlexDom([0]*dim, [1]*dim, [2]*dim)
-#         self.dom2d.setFemIndexing(2)
-#         spectral2D = Spectral(2,2)
-#         self.dom2d.computeFullCoordinates(spectral2D)
+    def setUp(self):
+        ngl = 2
+        data2D = {'lower': [0,0] , 'upper':[1,1], "nelem": [2,2]}
+        self.dom = DMPlexDom(boxMesh=data2D)
+        self.dom.setFemIndexing(ngl)
 
-#         self.testVelVec = self.dom2d.createGlobalVec()
-#         self.dim = dim
+        dim = self.dom.getDimension()
+        spectral2D = Spectral(ngl,dim)
+        self.dom.computeFullCoordinates(spectral2D)
 
-#     def test_get_all_global_nodes_2D(self):
-#         allNodes = self.dom2d.getAllNodes()
-#         self.assertEqual(len(allNodes), 9)
+        self.testVelVec = self.dom.createGlobalVec()
 
-#     def test_get_all_global_nodes_2D_ngls(self):
-#         dim = 2
-#         ngl = 3
-#         dom = DMPlexDom([0]*dim, [1]*dim, [2]*dim)
-#         dom.setFemIndexing(ngl)
-#         allNodes = dom.getAllNodes()
-#         self.assertEqual(len(allNodes), 25)
+    def test_get_all_global_nodes_ngls(self):
+        data2D = {'lower': [0,0] , 'upper':[0.6,0.8], "nelem": [2,3]}
+        for ngl in range(2, 14):
+            dom = DMPlexDom(boxMesh=data2D)
+            dom.setFemIndexing(ngl)
+            allNodes = dom.getAllNodes()
+            total = 12 + 17*(ngl-2) + 6*((ngl-2)**2)
+            assert len(allNodes) == total
+            del dom
 
-#     def test_get_nodes_coordinates_2D(self):
-#         allNodes = self.dom2d.getAllNodes()
-#         coords = [[0., 0. ], [0.5, 0. ], [1.,  0. ], [0.,  0.5], [0.5, 0.5], [1.,  0.5], [0.,  1. ], [0.5, 1. ], [1.,  1. ]]
-#         test_coords = self.dom2d.getNodesCoordinates(allNodes)
-#         np_test.assert_array_almost_equal(coords, test_coords)
+    def test_get_nodes_coordinates_2D(self):
+        allNodes = self.dom.getAllNodes()
+        coords = [[0., 0. ], [0.5, 0. ], [1.,  0. ], [0.,  0.5], [0.5, 0.5], [1.,  0.5], [0.,  1. ], [0.5, 1. ], [1.,  1. ]]
+        test_coords = self.dom.getNodesCoordinates(allNodes)
+        np_test.assert_array_almost_equal(coords, test_coords)
 
 #     # TODO: implement 3D tests
 
-#     def test_set_function_vec_to_vec_2D(self):
-#         np_coords = np.array([[0., 0. ], [0.5, 0. ], [1.,  0. ], [0.,  0.5], [0.5, 0.5], [1.,  0.5], [0.,  1. ], [0.5, 1. ], [1.,  1. ]])
-#         result = np.sqrt(np_coords)
-#         allNodes = self.dom2d.getAllNodes()
-#         f = lambda coords : (sqrt(coords[0]),sqrt(coords[1]))
-#         self.dom2d.applyFunctionVecToVec(allNodes, f, self.testVelVec,self.dim)
-#         test_result = self.testVelVec.getArray().reshape(9,2)
-#         np_test.assert_array_almost_equal(result, test_result, decimal=12)
+    def test_set_function_vec_to_vec_2D(self):
+        np_coords = np.array([[0., 0. ], [0.5, 0. ], [1.,  0. ], [0.,  0.5], [0.5, 0.5], [1.,  0.5], [0.,  1. ], [0.5, 1. ], [1.,  1. ]])
+        result = np.sqrt(np_coords)
+        allNodes = self.dom.getAllNodes()
 
-#     def test_set_function_vec_to_vec_2D_some_nodes(self):
-#         np_coords = np.array([[0., 0. ], [0, 0. ], [0.,  0. ], [0.,  0.5], [0., 0.], [0.,  0.], [0.,  0. ], [0.5, 1. ], [0.,  0. ]])
-#         result = np.sqrt(np_coords)
-#         self.testVelVec.set(0.0)
-#         someNodes = [0,3,7] 
-#         f = lambda coords : (sqrt(coords[0]),sqrt(coords[1]))
-#         self.dom2d.applyFunctionVecToVec(someNodes, f, self.testVelVec,self.dim)
-#         test_result = self.testVelVec.getArray().reshape(9,2)
-#         np_test.assert_array_almost_equal(result, test_result, decimal=12)
+        f = lambda coords : (sqrt(coords[0]),sqrt(coords[1]))
+        dim = self.dom.getDimension()
+        self.dom.applyFunctionVecToVec(allNodes, f, self.testVelVec,dim)
+        test_result = self.testVelVec.getArray().reshape(9,2)
+        np_test.assert_array_almost_equal(result, test_result, decimal=12)
 
-#     def test_set_function_scalar_to_vec_2D(self):
-#         vecScalar = PETSc.Vec().createSeq(9)
-#         result = np.array([0,0.5,1,0.5,1.,1.5,1,1.5,2])
-#         allNodes = self.dom2d.getAllNodes()
-#         f = lambda coord: (coord[0]+coord[1])
-#         self.dom2d.applyFunctionScalarToVec(allNodes, f , vecScalar)
-#         test_result = vecScalar.getArray()
-#         np_test.assert_array_almost_equal(result, test_result, decimal=12)
+    def test_set_function_vec_to_vec_2D_some_nodes(self):
+        np_coords = np.array([[0., 0. ], [1, 1. ], [1.,  1. ], [0.,  0.5], [1., 1.], [1.,  1.], [1.,  1. ], [0.5, 1. ], [1.,  1. ]])
+        result = np.sqrt(np_coords)
 
-#     def test_set_constant_to_vec_2D(self):
-#         result = np.array( [3,5]* 9).reshape(9, 2)
-#         vec = PETSc.Vec().createSeq(18)
-#         self.dom2d.applyValuesToVec(self.dom2d.getAllNodes(), [3,5], vec)
-#         test_result = vec.getArray().reshape(9,2)
-#         np_test.assert_array_almost_equal(result, test_result, decimal=12)
+        self.testVelVec.set(1.0)
+        someNodes = [0,3,7] 
+        f = lambda coords : (sqrt(coords[0]),sqrt(coords[1]))
+
+        dim = self.dom.getDimension()
+        self.dom.applyFunctionVecToVec(someNodes, f, self.testVelVec, dim)
+        test_result = self.testVelVec.getArray().reshape(9,2)
+        np_test.assert_array_almost_equal(result, test_result, decimal=12)
+
+    def test_set_function_scalar_to_vec_2D(self):
+        vecScalar = PETSc.Vec().createSeq(9)
+
+        result = np.array([0,0.5,1,0.5,1.,1.5,1,1.5,2])
+
+        allNodes = self.dom.getAllNodes()
+        f = lambda coord: (coord[0]+coord[1])
+        self.dom.applyFunctionScalarToVec(allNodes, f , vecScalar)
+        test_result = vecScalar.getArray()
+        np_test.assert_array_almost_equal(result, test_result, decimal=12)
+
+    def test_set_constant_to_vec_2D(self):
+
+        cteValue = [3 , 5]
+
+        result = np.array( [3,5]* 9).reshape(9, 2)
+        vec = PETSc.Vec().createSeq(18)
+        self.dom.applyValuesToVec(self.dom.getAllNodes(), cteValue, vec)
+        test_result = vec.getArray().reshape(9,2)
+        np_test.assert_array_almost_equal(result, test_result, decimal=12)
 
     # def test_set_function_to_vec(self):
     #     raise NotImplementedError
