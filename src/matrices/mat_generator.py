8e7da737c325eb6bc13d9fb101cc05f185c38836
Ignacio Bejarano <ignbejarano@gmail.com>
2020-12-09 08:52:58 -0300

    create nnz ind with arrays

--- src/matrices/mat_generator.py
+++ src/matrices/mat_generator.py
@@ -2,7 +2,7 @@ from petsc4py import PETSc
 import numpy as np
 
 class Mat:
-    def __init__(self, dim, comm):
+    def __init__(self, dim, comm=PETSc.COMM_WORLD):
         self.dim = dim
         self.dim_w = 1 if self.dim == 2 else 3
         self.dim_s = 3 if self.dim == 2 else 6
@@ -13,27 +13,35 @@ class Mat:
         self.Rw.assemble()
         self.Rd.assemble()
         self.Krhs.assemble()
+
+    def isParallel(self):
+        return self.comm.rank > 1
+
+    def getGlobalIndices(self, localIndices):
+        globalIndices = set()
+        if self.isParallel():
+            collectIndices = self.comm.allgather([localIndices])
+            for remoteIndices in collectIndices:
+                globalIndices |= remoteIndices[0]
+        else:
+            globalIndices = localIndices
+        return globalIndices
         
     def createEmptyKLEMats(self,rStart, rEnd ,  d_nnz_ind , o_nnz_ind, ind_d, ind_o, indicesDIR):
-        self.globalIndicesDIR =set()
-
-        collectIndices = self.comm.allgather([indicesDIR])
-        for remoteIndices in collectIndices:
-            self.globalIndicesDIR |= remoteIndices[0]
-
+        self.globalIndicesDIR = self.getGlobalIndices(indicesDIR)
         locElRow = rEnd - rStart
         vel_dofs = locElRow * self.dim
         vort_dofs = locElRow * self.dim_w
         # Create matrices for the resolution of the KLE and vorticity transport
-        # Create list of NNZ from d_nnz_ind and o_nnz_ind to create K
-        d_nnz, o_nnz = self.createNonZeroIndex(d_nnz_ind, o_nnz_ind, self.dim, self.dim )
-        # Create list of NNZ from d_nnz_ind and o_nnz_ind to create Rw
-        dw_nnz, ow_nnz = self.createNonZeroIndex(d_nnz_ind, o_nnz_ind, self.dim_w, self.dim)
-        # Create list of NNZ from d_nnz_ind and o_nnz_ind to create Rd
-        dd_nnz, od_nnz = self.createNonZeroIndex(d_nnz_ind, o_nnz_ind, 1, self.dim)
+        # Create array of NNZ from d_nnz_ind and o_nnz_ind to create K
+        d_nnz, o_nnz = self.createNNZWithArray(d_nnz_ind, o_nnz_ind, self.dim, self.dim )
+        # Create array of NNZ from d_nnz_ind and o_nnz_ind to create Rw
+        dw_nnz, ow_nnz = self.createNNZWithArray(d_nnz_ind, o_nnz_ind, self.dim_w, self.dim)
+        # Create array of NNZ from d_nnz_ind and o_nnz_ind to create Rd
+        dd_nnz, od_nnz = self.createNNZWithArray(d_nnz_ind, o_nnz_ind, 1, self.dim)
 
-        drhs_nnz_ind = [0] * locElRow
-        orhs_nnz_ind = [0] * locElRow
+        drhs_nnz_ind = np.zeros(locElRow)
+        orhs_nnz_ind = np.zeros(locElRow)
 
         for indRow, indSet in enumerate(ind_d):
             if (indRow + rStart) in self.globalIndicesDIR:
@@ -45,7 +53,7 @@ class Mat:
 
         # FIXME: This reserves self.dim nonzeros for each node with
         # Dirichlet conditions despite the number of DoF conditioned
-        drhs_nnz, orhs_nnz = self.createNonZeroIndex(drhs_nnz_ind, orhs_nnz_ind, self.dim, self.dim)
+        drhs_nnz, orhs_nnz = self.createNNZWithArray(drhs_nnz_ind, orhs_nnz_ind, self.dim, self.dim)
 
         for indRow in set(range(rStart, rEnd)) & set(indicesDIR):
             minInd = (indRow - rStart) * self.dim
@@ -75,6 +83,13 @@ class Mat:
         di_nnz = [x * dim1 for x in d_nnz for d in range(dim2)]
         oi_nnz = [x * dim1 for x in o_nnz for d in range(dim2)]
         return di_nnz, oi_nnz
+
+    def createNNZWithArray(self, d_nnz: np.array, o_nnz: np.array, dim1: int, dim2: int):
+        d_nnz = np.array(d_nnz, dtype=np.int32)
+        o_nnz = np.array(o_nnz, dtype=np.int32)
+        di_nnz_arr = np.repeat(d_nnz*dim1, dim2)
+        oi_nnz_arr = np.repeat(o_nnz*dim1, dim2)
+        return di_nnz_arr, oi_nnz_arr
 
     def setIndices2One(self, indices2one):
         for indd in indices2one:
@@ -141,3 +156,7 @@ class Operators(Mat):
         self.weigSrT.destroy()
         self.weigCurl.destroy()
         self.weigDivSrT.destroy()
+
+
+if  __name__ == "__main__":
+    mat = Mat(2)
